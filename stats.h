274c9f336689c9c52325fa8bcf382bc06bee4654
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-10-12 14:26:40 +0200

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,13 +1,12 @@
 #include <vector>
-
-namespace Statistics {
-    Stats ComputeStatistics(const std::vector<float>& );
-    
-    class stats
+class Stats
     {
         public:float average=0;
                float max=0;
                float min=0;
     };
+namespace Statistics {
+    Stats ComputeStatistics(const std::vector<float>& );
+    
     
 }
