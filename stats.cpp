24aad243a708b78f8a3372271b188406ec0a2171
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-10-12 14:14:01 +0200

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,32 @@
 #include "stats.h"
+#include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& vec) {
     //Implement statistics here
+    stats s;
+    int i;
+    float sum=0,max=0,min=0;
+    if(vec.size()==0)
+    {
+        s.average =NAN;
+        s.max=NAN;
+        s.min=NAN;
+    }
+    else
+    {
+        s.max=vec[0];
+        s.min=vec[0];
+        for(i = vec.begin(); i != vec.end(); ++i)
+         {
+            sum+=i;
+            if(min>i)
+                min=i;
+            if(max<i)
+                max=i;
+          }
+        s.average =sum/vec.size();
+        s.max=max;
+        s.min=min;
+     }
+    return s;
 }
