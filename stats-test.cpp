8eb3799e88498868d8b161d727f05bacbecc95ba
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-10-22 20:57:15 +0200

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -4,7 +4,30 @@
 #include "stats.h"
 
 #include <cmath>
-
+class StatsAlerter {
+    public:
+    float maxT;
+//     float maxe;
+    std::vector<IAlerter*> m_alert;
+    
+    // constructor
+    StatsAlerter(const float maxThreshold, std::vector<IAlerter*> alerters) 
+    {maxT = maxThreshold; 
+      m_alert = alerters;}
+    
+    void checkAndAlert(std::vector<float> vals ) 
+    {
+        auto comStat = Statistics::ComputeStatistics(vals);
+        if (comStat.max > maxT)
+        {
+            for (size_t i = 0; i < alerters.size(); i++)
+        {
+            m_alert.at(i)->alerters();
+        }
+        }
+//         maxe = vals.at(0);
+    }
+    
 TEST_CASE("reports average, minimum and maximum") {
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
