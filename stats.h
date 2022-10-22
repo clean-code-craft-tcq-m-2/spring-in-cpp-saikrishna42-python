e971cbb3548b23172e78d4bbe7c1ce6f8d6100aa
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-10-22 20:46:33 +0200

    Update stats.h

--- stats.h
+++ stats.h
@@ -8,7 +8,7 @@ class Stats
 class IAlerter
 {
     public: void alerters ();
-}
+};
 class EmailAlert : public IAlerter
     {
         public:int emailSent=1;
@@ -18,6 +18,30 @@ class EmailAlert : public IAlerter
     {
         public:int ledGlows=1;
     };
+class StatsAlerter {
+    public:
+    float maxT;
+//     float maxe;
+    std::vector<IAlerter*> m_alert;
+    
+    // constructor
+    StatsAlerter(const float maxThreshold, std::vector<IAlerter*> alerter) 
+    {maxT = maxThreshold; 
+      m_alert = alerter;}
+    
+    void checkAndAlert(std::vector<float> vals ) 
+    {
+        auto comStat = Statistics::ComputeStatistics(vals);
+        if (comStat.max > maxT)
+        {
+            for (size_t i = 0; i < m_alert.size(); i++)
+        {
+            m_alert.at(i)->alert();
+        }
+        }
+//         maxe = vals.at(0);
+    }
+};
 
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& );
